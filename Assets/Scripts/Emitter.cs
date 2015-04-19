42c228e99b0c719f6358932283f1769c9701fd1a
LiamAttClarke <liam.att.clarke@gmail.com>
2015-04-18 23:12:27 -0400

    Fixed reflection/ raycasting.

--- Assets/Scripts/Emitter.cs
+++ Assets/Scripts/Emitter.cs
@@ -7,108 +7,99 @@ public class Emitter : MonoBehaviour
     LineRenderer line;
     List<Vector3> list = new List<Vector3>();
     Vector3 initialLightDir;
-    bool isOutsideScreen;
-    RaycastHit2D? pastHit;
-    Vector3 prevPos;
+	RaycastHit2D hit;
+	RaycastHit2D[] hits;
+	bool isReflection = false;
+
     // Use this for initialization
     void Start()
     {
         line = GetComponent<LineRenderer>();
-        line.SetVertexCount(1);
-        line.SetPosition(0, transform.position);
         initialLightDir = new Vector3(1, 1);
-        isOutsideScreen = false;
-        prevPos = transform.position;
-        //SetPoint(transform.position, initialLightDir);
-    }
+		list.Add (transform.position);
+		SetPoint (transform.position, initialLightDir);
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (prevPos != transform.position)
-        {
-            prevPos = transform.position;
-            list.Clear();
-            list.Add(transform.position);
-            isOutsideScreen = false;
-            pastHit = null;
-        }
-        Vector3 cursorPos = Input.mousePosition;
-        Vector3 clickPosAtCamera = Camera.main.ScreenToWorldPoint(cursorPos);
-        Vector3 clickPos = new Vector3(clickPosAtCamera.x, clickPosAtCamera.y, transform.position.z);
-        SetPoint(transform.position, initialLightDir);
-        MakeLine();
-
+		SetLine ();
     }
-    RaycastHit2D hit;
+
     //is called recursively to draw the beam of light
-    public void SetPoint(Vector3 origin, Vector3 direction)
+    void SetPoint(Vector3 origin, Vector3 direction)
     {
-        hit = Physics2D.Raycast(origin, direction);
-        Debug.Log(hit.distance);
-        if (!isOutsideScreen) {
-            if (hit.collider != null) {
-                if (hit == pastHit || ! (pastHit == null)) {
-                    return;
-                }
-                else {
-                    switch (hit.collider.tag) {
-                        case "Solid":
-                            list.Add(hit.point);
-                            break;
-                        case "Reflect":
-                            list.Add(hit.point);
-                            //Reflect(hit, direction);
-                            Vector3 v = Vector3.Reflect(direction, hit.normal);
-                            SetPoint(hit.point, v);
-                            break;
-                        case "Refract":
-                            list.Add(hit.point);
-                            Refract(hit, direction);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                pastHit = hit;
-            }
-            else
-            {
-                list.Add(origin + direction * Screen.width);
-                isOutsideScreen = true;
-            }
-        }
-        else return;
-    }
-
-    //calls setPoint at the end
-    private void Reflect(RaycastHit2D hit, Vector3 direction) {
+		if (isReflection) {
+			hits = Physics2D.RaycastAll (origin, direction);
+			if (hits.Length > 1) {
+				switch (hits[1].collider.tag) {
+				case "Solid":
+					list.Add (hits[1].point);
+					break;
+				case "Reflect":
+					list.Add (hits[1].point);
+					isReflection = true;
+					Vector3 reflection = Reflect (hits[1], direction);
+					SetPoint (hits[1].point, reflection);
+					break;
+				case "Refract":
+					list.Add (hits[1].point);
+					//Vector3 refraction = Refract (hit, direction);
+					SetPoint (hits[1].point, new Vector3 (0, 1, 0));
+					break;
+				default:
+					Debug.Log ("default");
+					break;
+				}
+				isReflection = false;
+			} else {
+				list.Add (direction * 20);
+			}
+		} else {
+			hit = Physics2D.Raycast (origin, direction);
+			if (hit.collider != null) {
+				switch (hit.collider.tag) {
+				case "Solid":
+					list.Add (hit.point);
+					break;
+				case "Reflect":
+					list.Add (hit.point);
+					isReflection = true;
+					Vector3 reflection = Reflect (hit, direction);
+					SetPoint (hit.point, reflection);
+					break;
+				case "Refract":
+					list.Add (hit.point);
+					//Vector3 refraction = Refract (hit, direction);
+					SetPoint (hit.point, new Vector3 (0, 1, 0));
+					break;
+				default:
+					Debug.Log ("default");
+					break;
+				}
+			} else {
+				list.Add (direction * 20);
+			}
+		}
+	}
+	
+	Vector3 Reflect(RaycastHit2D hit, Vector3 direction) {
         Vector2 normal = hit.normal;
         Vector2 incidentVect = direction.normalized;
         Vector2 reflectedVect = incidentVect - 2 * (Vector2.Dot(incidentVect, normal)) * normal;
-        SetPoint(hit.point, reflectedVect);
-        //list.Add(reflectedVect * 10);
+		return reflectedVect;
     }
 
-    // calls setPoint
-    private void Refract(RaycastHit2D hit, Vector3 direction) {
-        float refractionIndexAir = 1.0f;
-        float refractionIndexGlass = 1.5f;
-        Vector2 incidentVect = (Vector2)direction.normalized;
-        Vector2 normal = hit.normal;
-        float theta = Vector2.Angle(incidentVect, normal);
-        float refractionAngle = Mathf.Asin(refractionIndexAir / refractionIndexGlass * Mathf.Sin(theta));
-        Vector3 refractionVect = new Vector3(Mathf.Cos(refractionAngle), Mathf.Sin(refractionAngle), 0);
-        SetPoint(hit.point, refractionVect);
+    Vector3 Refract(RaycastHit2D hit, Vector3 direction) {
+		return direction + new Vector3(1,2,3);
     }
 
-    private void MakeLine() {
-        line.SetVertexCount(list.Count + 1);
+    void SetLine() {
+        line.SetVertexCount(list.Count);
         line.SetPosition(0, transform.position);
-        for (int i = 0; i < list.Count; i++)
+        for (int i = 1; i < list.Count; i++)
         {
-            line.SetPosition(i + 1, list[i]);
+            line.SetPosition(i, list[i]);
         }
     }
 
